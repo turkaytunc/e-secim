15b2ca4031ff851a0bc77ee83aac6da3d695f18b
turkaytunc <turkaytunc00@gmail.com>
2020-12-20 15:00:37 +0300

    update: overall style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './header.scss';
+import vote from '../../static-files/vote.jpg';
 
 const Header = (): JSX.Element => {
   return (
     <nav className="header-container">
-      <div className="corp-logo-container">Logo</div>
+      <div className="corp-logo-container">
+        <img
+          style={{ borderRadius: '50%', height: '100px', width: '100px', boxShadow: '1px 1px 3px #333' }}
+          src={vote}
+          alt=""
+        />
+      </div>
       <div className="project-title">Elektronik Seçim Uygulamasına Hoş Geldiniz.</div>
       <ul className="nav-link-container">
         <li>
