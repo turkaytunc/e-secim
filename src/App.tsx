d860f9d74143ccab0bf416bdbdf2b4ebecda3728
turkaytunc <turkaytunc00@gmail.com>
2020-12-20 14:44:13 +0300

    update: style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './app.scss';
 import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 
 function App(): JSX.Element {
   return (
     <div className="app">
+      <Header />
+      <main>Main Content</main>
       <Footer />
     </div>
   );
