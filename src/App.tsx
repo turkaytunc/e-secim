35b4740500b9f09fd1f66ca52ca4de281effdd4d
turkaytunc <turkaytunc00@gmail.com>
2020-12-20 13:52:02 +0300

    add: footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-
 import './app.scss';
+import Footer from './components/footer/Footer';
 
-function App() {
-  return <div className="app"></div>;
+function App(): JSX.Element {
+  return (
+    <div className="app">
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <ul>
+        <li>SSS</li>
+      </ul>
+      <ul>
+        <li>&copy; 2020 Turkay Tunc</li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
