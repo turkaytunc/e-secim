303690e8bc259ffc29d806ad0a0ad97695b8017b
turkaytunc <turkaytunc00@gmail.com>
2020-12-20 14:36:30 +0300

    add: navigation bar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import './header.scss';
 
-const Header = () => {
-  return <nav className="header-container">Navigation</nav>;
+const Header = (): JSX.Element => {
+  return (
+    <nav className="header-container">
+      <div className="corp-logo-container">Logo</div>
+      <div className="project-title">Elektronik Seçim Uygulamasına Hoş Geldiniz.</div>
+      <ul className="nav-link-container">
+        <li>
+          <input className="nav-search-box" type="search" placeholder="Ara" />
+        </li>
+        <li>link1</li>
+        <li>link2</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Header;
