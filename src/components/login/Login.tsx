f8be907f2ded2867f61b053dcc0182de1c2f0fed
turkaytunc <turkaytunc00@gmail.com>
2020-12-22 20:09:04 +0300

    add: input handler functions and states

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './login.scss';
 
 const Login = () => {
-  const handleSubmit = () => {};
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    console.log(userId, userPassword);
+    setUserId('');
+    setUserPassword('');
+  };
   return (
-    <form onSubmit={() => handleSubmit()} className="login-form-container">
+    <form onSubmit={(event: React.FormEvent<HTMLFormElement>) => handleSubmit(event)} className="login-form-container">
       <div className="login-page-container">
         <div className="login-label-container">
           <label className="login-label" htmlFor="tc-kimlik-no">
@@ -15,8 +24,20 @@ const Login = () => {
           </label>
         </div>
         <div className="login-input-container">
-          <input type="text" name="tc-kimlik-no" id="tc-kimlik-no" />
-          <input type="password" name="sifre" id="sifre" />
+          <input
+            type="text"
+            name="tc-kimlik-no"
+            id="tc-kimlik-no"
+            value={userId}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserId(event.target.value)}
+          />
+          <input
+            type="password"
+            name="sifre"
+            id="sifre"
+            value={userPassword}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserPassword(event.target.value)}
+          />
         </div>
       </div>
       <div className="submit-button-container">
