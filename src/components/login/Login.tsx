cd9a5f3455ebd44b42035b4cfd6b5068ac639191
turkaytunc <turkaytunc00@gmail.com>
2020-12-22 19:57:49 +0300

    add: styles to form inputs

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import './login.scss';
 
 const Login = () => {
   const handleSubmit = () => {};
   return (
-    <div className="login-page-container">
-      <form onSubmit={() => handleSubmit()}>
-        <label className="login-label">
-          TC Kimlik No <input type="text" name="tc-kimlik-no" id="tc-kimlik-no" />
-        </label>
-        <label className="login-label">
-          Şifre <input type="password" name="sifre" id="sifre" />
-        </label>
-      </form>
-    </div>
+    <form onSubmit={() => handleSubmit()} className="login-form-container">
+      <div className="login-page-container">
+        <div className="login-label-container">
+          <label className="login-label" htmlFor="tc-kimlik-no">
+            TC Kimlik No
+          </label>
+          <label className="login-label" htmlFor="sifre">
+            Şifre
+          </label>
+        </div>
+        <div className="login-input-container">
+          <input type="text" name="tc-kimlik-no" id="tc-kimlik-no" />
+          <input type="password" name="sifre" id="sifre" />
+        </div>
+      </div>
+      <div className="submit-button-container">
+        <button type="submit">Giriş</button>
+      </div>
+    </form>
   );
 };
 
