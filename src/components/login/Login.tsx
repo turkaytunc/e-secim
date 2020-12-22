8e47f3841a880bce96f5e8ee8eb832fa102ebf6b
turkaytunc <turkaytunc00@gmail.com>
2020-12-22 19:20:22 +0300

    add: login form inputs

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Login = () => {
-  return <div>Login Page</div>;
+  const handleSubmit = () => {};
+  return (
+    <div className="login-page-container">
+      <form onSubmit={() => handleSubmit()}>
+        <label className="login-label">
+          TC Kimlik No <input type="text" name="tc-kimlik-no" id="tc-kimlik-no" />
+        </label>
+        <label className="login-label">
+          Şifre <input type="password" name="sifre" id="sifre" />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
