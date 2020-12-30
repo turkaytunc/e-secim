2665f4984e1d27715a165f8a6bab9ce699bffc48
turkaytunc <turkaytunc00@gmail.com>
2020-12-30 18:40:15 +0300

    add: sha256 hashing to login and check user authorization

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -5,58 +5,65 @@ import sha256 from 'crypto-js/sha256';
 const Login = () => {
   const [userId, setUserId] = useState('');
   const [userPassword, setUserPassword] = useState('');
+  const [isUserValid, setIsUserValid] = useState(false);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    console.log(userId, userPassword);
-    fetch(`https://secim.webde.biz.tr/api/developerhelper/secmenekle`, {
+    let hashedUserInfo = sha256(userId + userPassword).toString();
+
+    fetch('https://secim.webde.biz.tr/api/secim/isuservalid', {
       method: 'POST',
-      mode: 'no-cors',
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json;charset=UTF-8',
       },
-      body: JSON.stringify({
-        tcNo: userId,
-        sifre: userPassword,
-      }),
-    }).then((res) => console.log(res));
-    setUserId('');
-    setUserPassword('');
+      body: JSON.stringify({ hashedUserInfo: hashedUserInfo }),
+    })
+      .then((res) => (res.status === 200 ? setIsUserValid(true) : setIsUserValid(false)))
+      .catch((err) => console.log(err));
   };
+
   return (
-    <form onSubmit={(event: React.FormEvent<HTMLFormElement>) => handleSubmit(event)} className="login-form-container">
-      <div className="login-page-container">
-        <div className="login-label-container">
-          <label className="login-label" htmlFor="tc-kimlik-no">
-            TC Kimlik No
-          </label>
-          <label className="login-label" htmlFor="sifre">
-            Şifre
-          </label>
-        </div>
-        <div className="login-input-container">
-          <input
-            type="text"
-            name="tc-kimlik-no"
-            id="tc-kimlik-no"
-            value={userId}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserId(event.target.value)}
-          />
-          <input
-            type="password"
-            name="sifre"
-            id="sifre"
-            value={userPassword}
-            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserPassword(event.target.value)}
-          />
-        </div>
-      </div>
-      <div className="submit-button-container">
-        <button type="submit">Giriş</button>
-      </div>
-    </form>
+    <>
+      {isUserValid ? (
+        'user is valid'
+      ) : (
+        <form
+          onSubmit={(event: React.FormEvent<HTMLFormElement>) => handleSubmit(event)}
+          className="login-form-container"
+        >
+          <div className="login-page-container">
+            <div className="login-label-container">
+              <label className="login-label" htmlFor="tc-kimlik-no">
+                TC Kimlik No
+              </label>
+              <label className="login-label" htmlFor="sifre">
+                Şifre
+              </label>
+            </div>
+            <div className="login-input-container">
+              <input
+                type="text"
+                name="tc-kimlik-no"
+                id="tc-kimlik-no"
+                value={userId}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserId(event.target.value)}
+              />
+              <input
+                type="password"
+                name="sifre"
+                id="sifre"
+                value={userPassword}
+                onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserPassword(event.target.value)}
+              />
+            </div>
+          </div>
+          <div className="submit-button-container">
+            <button type="submit">Giriş</button>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
