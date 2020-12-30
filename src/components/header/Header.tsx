b95f31b50ec86b956b9e3b31ba54f243bf0954f4
turkaytunc <turkaytunc00@gmail.com>
2020-12-30 22:13:20 +0300

    update: css layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,34 +6,26 @@ import { Link } from 'react-router-dom';
 const Header = (): JSX.Element => {
   return (
     <nav className="header-container">
-      <div className="corp-logo-container">
-        <img
-          style={{ borderRadius: '50%', height: '100px', width: '100px', boxShadow: '1px 1px 3px #333' }}
-          src={vote}
-          alt=""
-        />
+      <div className="corp-container">
+        <img src={vote} alt="corp-logo" />
+        <div className="project-title">Elektronik Seçim Uygulamasına Hoş Geldiniz.</div>
       </div>
-      <div className="project-title">Elektronik Seçim Uygulamasına Hoş Geldiniz.</div>
-      <ul className="nav-link-container">
-        <li>
-          <input className="nav-search-box" type="search" placeholder="Size nasıl yardımcı olabilirim?" />
-        </li>
-        <li>
-          <Link to="/e-secim/">
-            <button className="nav-button homepage-button">Anasayfa</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/e-secim/signup">
-            <button className="nav-button">Üye Ol</button>
-          </Link>
-        </li>
-        <li>
-          <Link to="/e-secim/login">
-            <button className="nav-button login-button">Giriş Yap</button>
-          </Link>
-        </li>
-      </ul>
+      <input className="nav-search-box" type="search" placeholder="Size nasıl yardımcı olabilirim?" />
+      <div className="homepage-button-container">
+        <Link to="/e-secim/">
+          <button className="nav-button">Anasayfa</button>
+        </Link>
+      </div>
+      <div className="signup-button-container">
+        <Link to="/e-secim/signup">
+          <button className="nav-button">Üye Ol</button>
+        </Link>
+      </div>
+      <div className="login-button-container">
+        <Link to="/e-secim/login">
+          <button className="nav-button">Giriş Yap</button>
+        </Link>
+      </div>
     </nav>
   );
 };
