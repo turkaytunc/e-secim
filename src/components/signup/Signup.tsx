3d61b2cee43f91d84a123f0e5a2f39e1c20c7a8a
turkaytunc <turkaytunc00@gmail.com>
2020-12-30 16:04:49 +0300

    add: signup component for testing

---
+++ src/components/signup/Signup.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './signup.scss';
+
+const Signup = () => {
+  const [userId, setUserId] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    fetch(`https://secim.webde.biz.tr/api/developerhelper/secmenekle`, {
+      method: 'POST',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        tcNo: userId,
+        sifre: userPassword,
+      }),
+    }).then((res) => console.log(res));
+    setUserId('');
+    setUserPassword('');
+  };
+  return (
+    <form onSubmit={(event: React.FormEvent<HTMLFormElement>) => handleSubmit(event)} className="signup-form-container">
+      <div className="signup-page-container">
+        <div className="signup-label-container">
+          <label className="signup-label" htmlFor="tc-kimlik-no">
+            TC Kimlik No
+          </label>
+          <label className="signup-label" htmlFor="sifre">
+            Şifre
+          </label>
+        </div>
+        <div className="signup-input-container">
+          <input
+            type="text"
+            name="tc-kimlik-no"
+            id="tc-kimlik-no"
+            value={userId}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserId(event.target.value)}
+          />
+          <input
+            type="password"
+            name="sifre"
+            id="sifre"
+            value={userPassword}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUserPassword(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="submit-button-container">
+        <button type="submit">Üye Ol</button>
+      </div>
+    </form>
+  );
+};
+
+export default Signup;
