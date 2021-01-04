ed96c707dcec7dfb74ef07bd8db45bf90dc2b070
turkaytunc <turkaytunc00@gmail.com>
2021-01-04 12:22:46 +0300

    refactor: signup function

---
+++ src/util/userSignUp.ts
@@ -0,0 +1,12 @@
+export const userSignUp = (userId: string, userPassword: string) =>
+  fetch(`https://secim.webde.biz.tr/api/developerhelper/secmenekle`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      tcNo: userId,
+      sifre: userPassword,
+    }),
+  });
