ef98f001db23ae6001453266d46dcce85c1f226c
turkaytunc <turkaytunc00@gmail.com>
2021-01-11 19:56:43 +0300

    add: routing for successful vote

--- src/components/vote/Vote.tsx
+++ src/components/vote/Vote.tsx
@@ -3,11 +3,14 @@ import { Store } from '../../store/Store';
 import { fetchCandidates } from '../../util/fetchCandidates';
 import sha256 from 'crypto-js/sha256';
 import { vote } from '../../util/vote';
+import { useHistory } from 'react-router-dom';
 
 const Vote = () => {
   const [candidates, setCandidates] = useState([]);
   const [selectedOption, setSelectedOption] = useState('');
   const { state } = useContext(Store);
+  const [voteResponseCode, setVoteResponseCode] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     fetchCandidates()
@@ -15,35 +18,43 @@ const Vote = () => {
       .catch((err) => console.log(err));
   });
 
-  useEffect(() => {
-    console.log(selectedOption);
-  }, [selectedOption]);
-
   const giveVote = () => {
     if (selectedOption === '') return;
-
     const voteHash = sha256(state.user.tc + selectedOption).toString();
-
-    vote(voteHash, state.user.tcHash).then((data) => console.log(data));
+    vote(voteHash, state.user.tcHash).then((res) => setVoteResponseCode(res.status.toString()));
   };
+
+  useEffect(() => {
+    if (voteResponseCode === '200') {
+      history.push('/e-secim/results');
+    }
+  }, [voteResponseCode, history]);
 
   return (
     <div>
-      {candidates.map((el: any) => (
-        <div>
-          <label>
-            <input
-              onChange={(event) => setSelectedOption(event.target.value)}
-              type="radio"
-              name="aday"
-              value={el.adayNo}
-              checked={selectedOption === `${el.adayNo}`}
-            />
-            {el.adayAd}
-          </label>
-        </div>
-      ))}
-      <button onClick={() => giveVote()}>Oy Ver</button>
+      {voteResponseCode === '' ? (
+        <>
+          {candidates.map((el: any) => (
+            <div>
+              <label>
+                <input
+                  onChange={(event) => setSelectedOption(event.target.value)}
+                  type="radio"
+                  name="aday"
+                  value={el.adayNo}
+                  checked={selectedOption === `${el.adayNo}`}
+                />
+                {el.adayAd}
+              </label>
+            </div>
+          ))}
+          <button onClick={() => giveVote()}>Oy Ver</button>
+        </>
+      ) : voteResponseCode === '400' ? (
+        'oy kullanma Hakki yok'
+      ) : (
+        'sonuclar ekrani'
+      )}
     </div>
   );
 };
