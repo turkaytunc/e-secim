6c66dc7bc5cbef3230860e4788f78b7e7cae6fce
turkaytunc <turkaytunc00@gmail.com>
2021-01-11 22:54:03 +0300

    add: bar chart for vote results

--- src/components/results/Results.tsx
+++ src/components/results/Results.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import VerticalBar from '../verticalBar/VerticalBar';
 import './results.scss';
 
 const Results = () => {
-  return <div className="results-container">Todo: sonuclari goruntule</div>;
+  const veri = [2, 3, 4];
+  const candidates = ['aday1', 'aday2', 'aday3'];
+  return (
+    <div className="results-container">
+      Todo: sonuclari goruntule
+      <VerticalBar voteData={veri} candidates={candidates} />
+    </div>
+  );
 };
 
 export default Results;
---
+++ src/components/verticalBar/VerticalBar.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Bar } from '@reactchartjs/react-chart.js';
+
+interface IProps {
+  voteData: number[];
+  candidates: string[];
+}
+
+const VerticalBar = ({ voteData, candidates }: IProps) => {
+  const data = {
+    labels: candidates,
+    datasets: [
+      {
+        label: 'Oylar',
+        data: voteData,
+        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <>
+      <div className="header"></div>
+      <Bar data={data} options={options} type="bar" />
+    </>
+  );
+};
+
+export default VerticalBar;
