0ff461631252a536cb3f4d76ec314be57f2d9451
turkaytunc <turkaytunc00@gmail.com>
2021-01-12 14:18:35 +0300

    add: transform transition to homepage

--- src/components/homepage/Homepage.tsx
+++ src/components/homepage/Homepage.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './homepage.scss';
 
 const Homepage = () => {
-  return <div className="homepage-project-title">Elektronik Seçim Uygulamasına Hoş Geldiniz.</div>;
+  return (
+    <div>
+      <div className="homepage-project-title">Elektronik Seçim Uygulamasına Hoş Geldiniz.</div>
+    </div>
+  );
 };
 
 export default Homepage;
