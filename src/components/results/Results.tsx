096098f735e963bfc21b19bce7332ced886ebb4b
turkaytunc <turkaytunc00@gmail.com>
2021-01-12 10:39:50 +0300

    add: result fetch funtion

--- src/components/results/Results.tsx
+++ src/components/results/Results.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import VerticalBar from '../verticalBar/VerticalBar';
 import './results.scss';
 
+type Result = [{ aday: { adayNo: number; adayAd: string }; adligiOySayisi: number }];
+
+const initialState: Result = [{ aday: { adayNo: 0, adayAd: 'Sonuclar Guncelleniyor' }, adligiOySayisi: 0 }];
+
 const Results = () => {
-  const veri = [2, 3, 4];
-  const candidates = ['aday1', 'aday2', 'aday3'];
+  const [result, setResult] = useState(initialState);
+
+  useEffect(() => {
+    const fetchResults = () =>
+      fetch('https://secim.webde.biz.tr/api/secim/secimsonuclari')
+        .then((data) => data.json())
+        .then((data) => setResult(data));
+
+    let time: NodeJS.Timeout;
+    time = setTimeout(() => {
+      fetchResults();
+    }, 5000);
+
+    return () => {
+      clearTimeout(time);
+    };
+  });
+
   return (
     <div className="results-container">
       Todo: sonuclari goruntule
-      <VerticalBar voteData={veri} candidates={candidates} />
+      <VerticalBar voteData={result.map((e) => e.adligiOySayisi)} candidates={result.map((e) => e.aday.adayAd)} />
     </div>
   );
 };
