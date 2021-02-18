34db1b188599c24cc637c8cc46a7beea9f4f0ba3
turkaytunc <turkaytunc00@gmail.com>
2021-02-18 16:35:31 +0300

    refactor: user validation function, now it handles error with try catch block

--- src/util/validateUser.ts
+++ src/util/validateUser.ts
@@ -1,8 +1,15 @@
-export const validateUser = (hashedUserInfo: string) =>
-  fetch('https://secim.webde.biz.tr/api/secim/isuservalid', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json;charset=UTF-8',
-    },
-    body: JSON.stringify({ hashedUserInfo: hashedUserInfo }),
-  });
+export const validateUser = async (hashedUserInfo: string) => {
+  try {
+    const fetchResult = await fetch('https://secim.webde.biz.tr/api/secim/isuservalid', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+      },
+      body: JSON.stringify({ hashedUserInfo: hashedUserInfo }),
+    });
+    return fetchResult.status;
+  } catch (error) {
+    console.error(error);
+    return 400;
+  }
+};
