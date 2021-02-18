4a2d3c9dcd02b7b3ba196776e926484b10e098dc
turkaytunc <turkaytunc00@gmail.com>
2021-02-18 23:30:30 +0300

    update: result screen tests, mock window fetch

--- src/__tests__/Results.test.tsx
+++ src/__tests__/Results.test.tsx
@@ -1,4 +1,4 @@
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, act } from '@testing-library/react';
 import Results from '../components/results/Results';
 import 'jest-canvas-mock';
 
@@ -7,9 +7,13 @@ jest.mock('@reactchartjs/react-chart.js', () => ({
 }));
 
 beforeEach(cleanup);
+beforeAll(() => jest.spyOn(window, 'fetch'));
 
 describe('<Results />', () => {
-  it('should render without crash', () => {
+  it('should render without crash', async () => {
+    (window.fetch as jest.Mock).mockResolvedValue({ json: () => Promise.resolve([]) });
+
     render(<Results />);
+    await act(() => Promise.resolve());
   });
 });
