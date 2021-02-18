19e389567ea11bad5975912a6212f38c6fa36c2a
turkaytunc <turkaytunc00@gmail.com>
2021-02-18 21:25:03 +0300

    update: vote function, now it uses mock fetch function

--- src/__tests__/util-tests/vote.test.ts
+++ src/__tests__/util-tests/vote.test.ts
@@ -1,7 +1,11 @@
 import { vote } from '../../util/vote';
+beforeAll(() => jest.spyOn(window, 'fetch'));
 
 describe('vote action handler', () => {
   it('hey', async () => {
+    (window.fetch as jest.Mock).mockResolvedValueOnce({
+      status: 400,
+    });
     return vote('12345678900', '1', '123hh2321h3h').then((res) => expect(res.status).toBe(400));
   });
 });
--- src/util/fetchCandidates.ts
+++ src/util/fetchCandidates.ts
@@ -1,11 +1,8 @@
 export const fetchCandidates = async () => {
-  let data = [];
   try {
     const fetchResult = await fetch('https://secim.webde.biz.tr/api/secim/getlistofaday');
-    data = await fetchResult.json();
-    return data;
+    return fetchResult.json();
   } catch (error) {
-    console.error('Cannot fetch candidates!');
-    return data;
+    return [];
   }
 };
